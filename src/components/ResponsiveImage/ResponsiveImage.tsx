b1b87646972fe27f65dac2da4ee0435bde211631
GuillermoCAB <guiga.benitez@gmail.com>
2023-06-27 17:47:41 -0300

    Add responsive image comp

---
+++ src/components/ResponsiveImage/ResponsiveImage.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+interface ResponsiveImageProps {
+  src: string
+  alt: string
+  width: number
+  height: number
+  className?: string
+  noLazy?: boolean
+}
+
+const ResponsiveImage: React.FC<ResponsiveImageProps> = ({
+  src,
+  alt,
+  width,
+  height,
+  className,
+  noLazy = false
+}) => {
+  const baseName = src.split('.').slice(0, -1).join('.')
+  const webpSrc = `${baseName}.webp`
+  const avifSrc = `${baseName}.avif`
+
+  return (
+    <div className={`responsive-image-wrapper ${className ?? ''}`}>
+      <picture>
+        <source type="image/avif" srcSet={avifSrc} />
+        <source type="image/webp" srcSet={webpSrc} />
+        <img
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          loading={noLazy ? 'eager' : 'lazy'}
+          decoding="async"
+        />
+      </picture>
+    </div>
+  )
+}
+
+export default ResponsiveImage
