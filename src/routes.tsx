887995d7d1ce7edba2b8108050c0153101fbcc5e
GuillermoCAB <guiga.benitez@gmail.com>
2023-07-06 20:40:33 -0300

    Small updates

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,7 @@
-import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// Pags
 import { Home } from './pages';
-
-// Components
-import SEO from './components/SEO';
+import { SEO } from './components';
 
 function AppRouter() {
   const router = createBrowserRouter([
