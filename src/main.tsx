6e3a15cd552185eef1a524aebf3a4af3a80418f1
GuillermoCAB <guiga.benitez@gmail.com>
2023-07-06 20:03:02 -0300

    Add global CSS config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import './main.css'
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <React.StrictMode>
